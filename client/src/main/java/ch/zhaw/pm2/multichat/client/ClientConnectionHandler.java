b845e739af7f881ff89d2262fba52278489f5346
Raphaël Monteiro <monterap@students.zhaw.ch>
2023-03-24 00:16:05 +0100

    initial commit

---
+++ client/src/main/java/ch/zhaw/pm2/multichat/client/ClientConnectionHandler.java
@@ -0,0 +1,223 @@
+package ch.zhaw.pm2.multichat.client;
+
+import ch.zhaw.pm2.multichat.protocol.ChatProtocolException;
+import ch.zhaw.pm2.multichat.protocol.NetworkHandler;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Scanner;
+
+import static ch.zhaw.pm2.multichat.client.ClientConnectionHandler.State.*;
+
+public class ClientConnectionHandler implements Runnable {
+
+    /** The network connection to be used for receiving and sending requests */
+    private final NetworkHandler.NetworkConnection<String> connection;
+
+    private final ChatWindowController controller;
+
+    // Data types used for the Chat Protocol
+    private static final String DATA_TYPE_CONNECT = "CONNECT";
+    private static final String DATA_TYPE_CONFIRM = "CONFIRM";
+    private static final String DATA_TYPE_DISCONNECT = "DISCONNECT";
+    private static final String DATA_TYPE_MESSAGE = "MESSAGE";
+    private static final String DATA_TYPE_ERROR = "ERROR";
+
+    public static final String USER_NONE = "";
+    public static final String USER_ALL = "*";
+
+    /** The username associated with this connection. */
+    private String userName = USER_NONE;
+
+    /** The current state of this connection */
+    private State state = NEW;
+
+    enum State {
+        NEW, CONFIRM_CONNECT, CONNECTED, CONFIRM_DISCONNECT, DISCONNECTED;
+    }
+
+    public ClientConnectionHandler(NetworkHandler.NetworkConnection<String> connection,
+                                   String userName,
+                                   ChatWindowController controller)  {
+        this.connection = connection;
+        this.userName = (userName == null || userName.isBlank())? USER_NONE : userName;
+        this.controller = controller;
+    }
+
+    public State getState() {
+        return this.state;
+    }
+
+    public void setState (State newState) {
+        this.state = newState;
+        controller.stateChanged(newState);
+    }
+
+    /**
+     * Start the connection handler.
+     * It will start listening for incoming messages from the server and process them.
+     */
+    @Override
+    public void run() {
+        System.out.println("Starting Connection Handler");
+        startReceiving();
+        System.out.println("Ended Connection Handler");
+    }
+
+    /**
+     * Terminate the Connection Handler by closing the connection to not receive any more messages.
+     */
+    public void terminate() {
+        System.out.println("Closing Connection Handler to Server");
+        stopReceiving();
+        System.out.println("Closed Connection Handler to Server");
+    }
+
+    private void startReceiving() {
+        try {
+            System.out.println("Start receiving data...");
+            while (connection.isAvailable()) {
+                String data = connection.receive();
+                processData(data);
+            }
+        } catch (SocketException e) {
+            System.out.println("Connection terminated locally");
+            this.setState(DISCONNECTED);
+            System.out.println("Unregistered because connection terminated" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("Connection terminated by remote peer");
+            this.setState(DISCONNECTED);
+            System.out.println("Unregistered because connection terminated" + e.getMessage());
+        } catch(IOException e) {
+            System.err.println("Communication error: " + e.getMessage());
+        } catch(ClassNotFoundException e) {
+            System.err.println("Received object of unknown type: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Stop receiving packages from the network connection, by closing the connection.
+     */
+    private void stopReceiving() {
+        try {
+            System.out.println("Stop receiving data...");
+            connection.close();
+            System.out.println("Stopped receiving data.");
+        } catch (IOException e) {
+            System.err.println("Failed to close connection." + e.getMessage());
+        }
+    }
+
+    private void processData(String data) {
+        try {
+            // parse data content
+            Scanner scanner = new Scanner(data);
+            String sender = null;
+            String reciever = null;
+            String type = null;
+            String payload = null;
+            if (scanner.hasNextLine()) {
+                sender = scanner.nextLine();
+            } else {
+                throw new ChatProtocolException("No Sender found");
+            }
+            if (scanner.hasNextLine()) {
+                reciever = scanner.nextLine();
+            } else {
+                throw new ChatProtocolException("No Reciever found");
+            }
+            if (scanner.hasNextLine()) {
+                type = scanner.nextLine();
+            } else {
+                throw new ChatProtocolException("No Type found");
+            }
+            if (scanner.hasNextLine()) {
+                payload = scanner.nextLine();
+            }
+
+            // dispatch operation based on type parameter
+            if (type.equals(DATA_TYPE_CONNECT)) {
+                System.err.println("Illegal connect request from server");
+            } else if (type.equals(DATA_TYPE_CONFIRM)) {
+                if (state == CONFIRM_CONNECT) {
+                    this.userName = reciever;
+                    controller.setUserName(userName);
+                    controller.setServerPort(connection.getRemotePort());
+                    controller.setServerAddress(connection.getRemoteHost());
+                    controller.addInfo(payload);
+                    System.out.println("CONFIRM: " + payload);
+                    this.setState(CONNECTED);
+                } else if (state == CONFIRM_DISCONNECT) {
+                    controller.addInfo(payload);
+                    System.out.println("CONFIRM: " + payload);
+                    this.setState(DISCONNECTED);
+                } else {
+                    System.err.println("Got unexpected confirm message: " + payload);
+                }
+            } else if (type.equals(DATA_TYPE_DISCONNECT)) {
+                if (state == DISCONNECTED) {
+                    System.out.println("DISCONNECT: Already in disconnected: " + payload);
+                    return;
+                }
+                controller.addInfo(payload);
+                System.out.println("DISCONNECT: " + payload);
+                this.setState(DISCONNECTED);
+            } else if (type.equals(DATA_TYPE_MESSAGE)) {
+                if (state != CONNECTED) {
+                    System.out.println("MESSAGE: Illegal state " + state + " for message: " + payload);
+                    return;
+                }
+                controller.addMessage(sender, reciever, payload);
+                System.out.println("MESSAGE: From " + sender + " to " + reciever + ": "+  payload);
+            } else if (type.equals(DATA_TYPE_ERROR)) {
+                controller.addError(payload);
+                System.out.println("ERROR: " + payload);
+            } else {
+                System.out.println("Unknown data type received: " + type);
+            }
+        } catch(ChatProtocolException error) {
+            System.err.println("Error while processing data: " + error.getMessage());
+            sendData(USER_NONE, userName, DATA_TYPE_ERROR, error.getMessage());
+        }
+    }
+
+    public void sendData(String sender, String receiver, String type, String payload) {
+        if (connection.isAvailable()) {
+            new StringBuilder();
+            String data = new StringBuilder()
+                .append(sender+"\n")
+                .append(receiver+"\n")
+                .append(type+"\n")
+                .append(payload+"\n")
+                .toString();
+            try {
+                connection.send(data);
+            } catch (SocketException e) {
+                System.err.println("Connection closed: " + e.getMessage());
+            } catch (EOFException e) {
+                System.err.println("Connection terminated by remote peer");
+            } catch(IOException e) {
+                System.err.println("Communication error: " + e.getMessage());
+            }
+        }
+    }
+
+    public void connect() throws ChatProtocolException {
+        if (state != NEW) throw new ChatProtocolException("Illegal state for connect: " + state);
+        this.sendData(userName, USER_NONE, DATA_TYPE_CONNECT,null);
+        this.setState(CONFIRM_CONNECT);
+    }
+
+    public void disconnect() throws ChatProtocolException {
+        if (state != NEW && state != CONNECTED) throw new ChatProtocolException("Illegal state for disconnect: " + state);
+        this.sendData(userName, USER_NONE, DATA_TYPE_DISCONNECT,null);
+        this.setState(CONFIRM_DISCONNECT);
+    }
+
+    public void message(String receiver, String message) throws ChatProtocolException {
+        if (state != CONNECTED) throw new ChatProtocolException("Illegal state for message: " + state);
+        this.sendData(userName, receiver, DATA_TYPE_MESSAGE,message);
+    }
+
+}
---
+++ server/src/main/java/ch/zhaw/pm2/multichat/server/ServerConnectionHandler.java
@@ -0,0 +1,201 @@
+package ch.zhaw.pm2.multichat.server;
+
+import ch.zhaw.pm2.multichat.protocol.ChatProtocolException;
+import ch.zhaw.pm2.multichat.protocol.NetworkHandler;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static ch.zhaw.pm2.multichat.server.ServerConnectionHandler.State.*;
+
+public class ServerConnectionHandler {
+    /** Global counter to generate connection IDs */
+    private static final AtomicInteger connectionCounter = new AtomicInteger(0);
+
+    /** The ID of this connection */
+    private final int connectionId = connectionCounter.incrementAndGet();
+
+    /** Reference to the registry managing all connections */
+    private final Map<String,ServerConnectionHandler> connectionRegistry;
+
+    /** The network connection to be used for receiving and sending requests */
+    private final NetworkHandler.NetworkConnection<String> connection;
+
+    // Data types used for the Chat Protocol
+    private static final String DATA_TYPE_CONNECT = "CONNECT";
+    private static final String DATA_TYPE_CONFIRM = "CONFIRM";
+    private static final String DATA_TYPE_DISCONNECT = "DISCONNECT";
+    private static final String DATA_TYPE_MESSAGE = "MESSAGE";
+    private static final String DATA_TYPE_ERROR = "ERROR";
+
+    private static final String USER_NONE = "";
+    private static final String USER_ALL = "*";
+
+    /**
+     * The username associated with this connection
+     * Using Anonymous-{@link #connectionId} if not specified by the client
+     */
+    private String userName = "Anonymous-"+connectionId;
+
+    /** The current state of this connection */
+    private State state = NEW;
+
+    enum State {
+        NEW, CONNECTED, DISCONNECTED;
+    }
+
+    public ServerConnectionHandler(NetworkHandler.NetworkConnection<String> connection,
+                                   Map<String,ServerConnectionHandler> registry) {
+        Objects.requireNonNull(connection, "Connection must not be null");
+        Objects.requireNonNull(registry, "Registry must not be null");
+        this.connection = connection;
+        this.connectionRegistry = registry;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    /**
+     * Start receiving packages from the network connection.
+     * It continuously receives packages from the network connection and processes it depending on the package type
+     */
+    public void startReceiving() {
+        System.out.println("Starting Connection Handler for " + userName);
+        try {
+            System.out.println("Start receiving data...");
+            while (connection.isAvailable()) {
+                String data = connection.receive();
+                processData(data);
+            }
+        } catch (SocketException e) {
+            System.out.println("Connection terminated locally");
+            connectionRegistry.remove(userName);
+            System.out.println("Unregistered because connection terminated: " + userName + " " + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("Connection terminated by remote peer");
+            connectionRegistry.remove(userName);
+            System.out.println("Unregistered because connection terminated: " + userName + " " + e.getMessage());
+        } catch(IOException e) {
+            System.err.println("Communication error: " + e.getMessage());
+        } catch(ClassNotFoundException e) {
+            System.err.println("Received object of unknown type: " + e.getMessage());
+        }
+        System.out.println("Ended Connection Handler for " + userName);
+    }
+
+    /**
+     * Stop receiving packages from the network connection, by closing the connection.
+     */
+    public void stopReceiving() {
+        System.out.println("Closing Connection Handler for " + userName + "...");
+        try {
+            System.out.println("Stop receiving data...");
+            connection.close();
+            System.out.println("Stopped receiving data.");
+        } catch (IOException e) {
+            System.err.println("Failed to close connection." + e.getMessage());
+        }
+        System.out.println("Closed Connection Handler for " + userName);
+    }
+
+    private void processData(String data) {
+        try {
+            // parse data content
+            Scanner scanner = new Scanner(data);
+            String sender = null;
+            String reciever = null;
+            String type = null;
+            String payload = null;
+            if (scanner.hasNextLine()) {
+                sender = scanner.nextLine();
+            } else {
+                throw new ChatProtocolException("No Sender found");
+            }
+            if (scanner.hasNextLine()) {
+                reciever = scanner.nextLine();
+            } else {
+                throw new ChatProtocolException("No Reciever found");
+            }
+            if (scanner.hasNextLine()) {
+                type = scanner.nextLine();
+            } else {
+                throw new ChatProtocolException("No Type found");
+            }
+            if (scanner.hasNextLine()) {
+                payload = scanner.nextLine();
+            }
+
+            // dispatch operation based on type parameter
+            if (type.equals(DATA_TYPE_CONNECT)) {
+                if (this.state != NEW) throw new ChatProtocolException("Illegal state for connect request: " + state);
+                if (sender == null || sender.isBlank()) sender = this.userName;
+                if (connectionRegistry.containsKey(sender))
+                    throw new ChatProtocolException("User name already taken: " + sender);
+                this.userName = sender;
+                connectionRegistry.put(userName, this);
+                sendData(USER_NONE, userName, DATA_TYPE_CONFIRM, "Registration successfull for " + userName);
+                this.state = CONNECTED;
+            } else if (type.equals(DATA_TYPE_CONFIRM)) {
+                System.out.println("Not expecting to receive a CONFIRM request from client");
+            } else if (type.equals(DATA_TYPE_DISCONNECT)) {
+                if (state == DISCONNECTED) {
+                    throw new ChatProtocolException("Illegal state for disconnect request: " + state);
+                }
+                if (state == CONNECTED) {
+                    connectionRegistry.remove(this.userName);
+                }
+                sendData(USER_NONE, userName, DATA_TYPE_CONFIRM, "Confirm disconnect of " + userName);
+                this.state = DISCONNECTED;
+                this.stopReceiving();
+            } else if (type.equals(DATA_TYPE_MESSAGE)) {
+                if (state != CONNECTED) throw new ChatProtocolException("Illegal state for message request: " + state);
+                if (USER_ALL.equals(reciever)) {
+                    for (ServerConnectionHandler handler : connectionRegistry.values()) {
+                        handler.sendData(sender, reciever, type, payload);
+                    }
+                } else {
+                    ServerConnectionHandler handler = connectionRegistry.get(reciever);
+                    if (handler != null) {
+                        handler.sendData(sender, reciever, type, payload);
+                    } else {
+                        this.sendData(USER_NONE, userName, DATA_TYPE_ERROR, "Unknown User: " + reciever);
+                    }
+                }
+            } else if (type.equals(DATA_TYPE_ERROR)) {
+                System.out.println("Received error from client (" + sender + "): " + payload);
+            } else {
+                System.out.println("Unknown data type received: " + type);
+            }
+        } catch(ChatProtocolException error) {
+            System.err.println("Error while processing data: " + error.getMessage());
+            sendData(USER_NONE, userName, DATA_TYPE_ERROR, error.getMessage());
+        }
+    }
+
+    public void sendData(String sender, String receiver, String type, String payload) {
+        if (connection.isAvailable()) {
+            new StringBuilder();
+            String data = new StringBuilder()
+                .append(sender+"\n")
+                .append(receiver+"\n")
+                .append(type+"\n")
+                .append(payload+"\n")
+                .toString();
+            try {
+                connection.send(data);
+            } catch (SocketException e) {
+                System.err.println("Connection closed: " + e.getMessage());
+            } catch (EOFException e) {
+                System.err.println("Connection terminated by remote peer");
+            } catch(IOException e) {
+                System.err.println("Communication error: " + e.getMessage());
+            }
+        }
+    }
+}
