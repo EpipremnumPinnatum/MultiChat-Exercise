9017ac9ec2ffa6ce41733181ff23f60cec87e715
Raphaël Monteiro <monterap@students.zhaw.ch>
2023-04-10 20:19:57 +0200

    added NetworkMessage class, contains the communication String between client and server

---
+++ protocol/src/main/java/ch/zhaw/pm2/multichat/protocol/NetworkMessage.java
@@ -0,0 +1,36 @@
+package ch.zhaw.pm2.multichat.protocol;
+
+public class NetworkMessage {
+    private final String sender;
+    private final String receiver;
+    private final Configuration.DataType type;
+    private final String payload;
+
+    public NetworkMessage(String sender, String receiver, Configuration.DataType type, String payload) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public Configuration.DataType getType() {
+        return type;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %s %s %s", sender, receiver, type, payload);
+    }
+}
