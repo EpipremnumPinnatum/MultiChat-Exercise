96aba59c41bc55a736c80e3e7cf2371515a8ca65
verdiant <verdiant@student.zhaw.ch>
2023-04-11 16:07:53 +0200

    Fix format and move private and public methods

--- client/src/main/java/ch/zhaw/pm2/multichat/client/ClientConnectionHandler.java
+++ client/src/main/java/ch/zhaw/pm2/multichat/client/ClientConnectionHandler.java
@@ -2,16 +2,13 @@ package ch.zhaw.pm2.multichat.client;
 
 import ch.zhaw.pm2.multichat.protocol.*;
 
-import static ch.zhaw.pm2.multichat.protocol.Configuration.DataType.*;
 import static ch.zhaw.pm2.multichat.protocol.Configuration.DataType;
+import static ch.zhaw.pm2.multichat.protocol.Configuration.DataType.*;
 import static ch.zhaw.pm2.multichat.protocol.Configuration.ProtocolState.*;
-
-//Todo: write javadoc
 
 /**
  * This class handles the communication with the server
  */
-
 public class ClientConnectionHandler extends ConnectionHandler implements Runnable {
     private final ChatWindowController controller;
 
@@ -24,11 +21,6 @@ public class ClientConnectionHandler extends ConnectionHandler implements Runnab
         this.controller = controller;
     }
 
-    public void setState(Configuration.ProtocolState newProtocolState) {
-        this.protocolState = newProtocolState;
-        controller.stateChanged(newProtocolState);
-    }
-
     /**
      * Start the connection handler.
      * It will start listening for incoming messages from the server and process them.
@@ -38,33 +30,6 @@ public class ClientConnectionHandler extends ConnectionHandler implements Runnab
         System.out.println("Starting Connection Handler");
         startReceiving();
         System.out.println("Ended Connection Handler");
-    }
-
-    /**
-     * Terminate the Connection Handler by closing the connection to not receive any more messages.
-     */
-    public void terminate() {
-        System.out.println("Closing Connection Handler to Server");
-        stopReceiving();
-        System.out.println("Closed Connection Handler to Server");
-    }
-
-    public void connect() throws ChatProtocolException {
-        if (protocolState != NEW) throw new ChatProtocolException("Illegal state for connect: " + protocolState);
-        this.sendData(userName, USER_NONE, CONNECT, null);
-        this.setState(CONFIRM_CONNECT);
-    }
-
-    public void disconnect() throws ChatProtocolException {
-        if (protocolState != NEW && protocolState != CONNECTED)
-            throw new ChatProtocolException("Illegal state for disconnect: " + protocolState);
-        this.sendData(userName, USER_NONE, DISCONNECT, null);
-        this.setState(CONFIRM_DISCONNECT);
-    }
-
-    public void message(String receiver, String message) throws ChatProtocolException {
-        if (protocolState != CONNECTED) throw new ChatProtocolException("Illegal state for message: " + protocolState);
-        this.sendData(userName, receiver, MESSAGE, message);
     }
 
     @Override
@@ -126,5 +91,37 @@ public class ClientConnectionHandler extends ConnectionHandler implements Runnab
     @Override
     protected void onInterrupted() {
         setState(DISCONNECTED);
+    }
+
+    public void setState(Configuration.ProtocolState newProtocolState) {
+        this.protocolState = newProtocolState;
+        controller.stateChanged(newProtocolState);
+    }
+
+    /**
+     * Terminate the Connection Handler by closing the connection to not receive any more messages.
+     */
+    public void terminate() {
+        System.out.println("Closing Connection Handler to Server");
+        stopReceiving();
+        System.out.println("Closed Connection Handler to Server");
+    }
+
+    public void connect() throws ChatProtocolException {
+        if (protocolState != NEW) throw new ChatProtocolException("Illegal state for connect: " + protocolState);
+        this.sendData(userName, USER_NONE, CONNECT, null);
+        this.setState(CONFIRM_CONNECT);
+    }
+
+    public void disconnect() throws ChatProtocolException {
+        if (protocolState != NEW && protocolState != CONNECTED)
+            throw new ChatProtocolException("Illegal state for disconnect: " + protocolState);
+        this.sendData(userName, USER_NONE, DISCONNECT, null);
+        this.setState(CONFIRM_DISCONNECT);
+    }
+
+    public void message(String receiver, String message) throws ChatProtocolException {
+        if (protocolState != CONNECTED) throw new ChatProtocolException("Illegal state for message: " + protocolState);
+        this.sendData(userName, receiver, MESSAGE, message);
     }
 }
