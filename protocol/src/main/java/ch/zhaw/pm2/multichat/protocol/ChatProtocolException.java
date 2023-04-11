bb761c488a9f297cf8c3d75a325e36816d49e77d
verdiant <verdiant@student.zhaw.ch>
2023-04-11 20:54:19 +0200

    Add Java Doc to methods and attributes

--- protocol/src/main/java/ch/zhaw/pm2/multichat/protocol/ChatProtocolException.java
+++ protocol/src/main/java/ch/zhaw/pm2/multichat/protocol/ChatProtocolException.java
@@ -1,7 +1,15 @@
 package ch.zhaw.pm2.multichat.protocol;
 
+/**
+ * An exception that represents a problem with the chat protocol.
+ */
 public class ChatProtocolException extends Exception {
 
+    /**
+     * Constructs a new `ChatProtocolException` with the specified detail message.
+     *
+     * @param message The detail message for the exception.
+     */
     public ChatProtocolException(String message) {
         super(message);
     }
